f44a6c60ad2e357aff8802698c0b9049da4acf98
Maxim Borisyak <mborisyak@yandex-team.ru>
2017-06-13 21:07:14 +0300

    generalized classification loss

--- crayimage/nn/objective/image.py
+++ crayimage/nn/objective/image.py
@@ -4,7 +4,7 @@ import theano
 import theano.tensor as T
 
 __all__ = [
-  'img_mse', 'plain_mse'
+  'img_mse', 'plain_mse', 'classification_loss'
 ]
 
 def border_mask(exclude_borders, img_shape, dtype='float32'):
@@ -25,6 +25,20 @@ def border_mask(exclude_borders, img_shape, dtype='float32'):
 
   return theano.shared(mask, name='border_excluding_mask')
 
+def mask(pixel_losses, exclude_borders=0, img_shape=None, norm=True, dtype='float32'):
+  if exclude_borders != 0:
+    mask = border_mask(exclude_borders, img_shape, dtype)
+    if norm:
+      norm_term = T.constant(np.sum(mask.get_value(), dtype=dtype))
+      return T.sum(mask[None, None, :, :] * pixel_losses, axis=(1, 2, 3)) / norm_term
+    else:
+      return T.sum(mask[None, None, :, :] * pixel_losses, axis=(1, 2, 3))
+  else:
+    if norm:
+      return T.mean(pixel_losses, axis=(1, 2, 3))
+    else:
+      return T.sum(pixel_losses, axis=(1, 2, 3))
+
 def img_mse(exclude_borders=0, img_shape=None, norm=True, dtype='float32'):
   if exclude_borders != 0:
     mask = border_mask(exclude_borders, img_shape, dtype)
@@ -40,3 +54,24 @@ def img_mse(exclude_borders=0, img_shape=None, norm=True, dtype='float32'):
       return lambda a, b: T.sum((a - b) ** 2, axis=(1, 2, 3))
 
 plain_mse = img_mse(exclude_borders=0, norm=False)
+
+def classification_loss(loss, predictions, targets, n_classes):
+  f = predictions - T.mean(predictions, axis=1, keepdims=True)
+
+  c = T.constant(-1.0 / (n_classes - 1), dtype=predictions.dtype)
+  y = targets + c * (1 - targets)
+
+  margins = T.mean(y * f, axis=1)
+
+  if hasattr(loss, '__call__'):
+    pass
+  elif loss == 'exp':
+    loss = lambda x: T.exp(-x)
+  elif loss == 'logit':
+    loss = lambda x: T.log(1 + T.exp(-x))
+  elif loss == 'l2':
+    loss = lambda x: (1 - x) ** 2
+  else:
+    raise Exception('`loss` must be either: callable, `exp`, `logit`, `l2`!')
+
+  return loss(margins)
