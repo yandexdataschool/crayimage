734bf4423afb7ec35c970bff684b7253849eb194
Maxim Borisyak <mborisyak@yandex-team.ru>
2017-09-07 17:24:59 +0300

    ...

--- crayimage/nn/subnetworks/fire.py
+++ crayimage/nn/subnetworks/fire.py
@@ -6,7 +6,8 @@ from lasagne import *
 from .. import layers as clayers
 
 __all__ = [
-  'fire_module'
+  'fire_module',
+  'freeze_module'
 ]
 
 def fire_module(
@@ -20,3 +21,14 @@ def fire_module(
   net = squeeze(incoming, n_filters / 4)
   expanded = [ exp(net, n_filters) for exp in expand ]
   return merge(expanded)
+
+def freeze_module(
+  incoming,
+  n_filters = 64,
+  squeeze=clayers.squeeze,
+  expand=(clayers.diff, clayers.diff1x1),
+  merge=clayers.concat
+):
+  expanded = [exp(incoming, n_filters) for exp in expand]
+  net = merge(expanded)
+  return squeeze(net)
